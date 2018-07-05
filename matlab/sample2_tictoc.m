207510d82e0aac388e01d6a880bdb88f54893b29
lukaspospisil <lukas.pospisil@usi.ch>
2018-07-05 18:21:42 +0200

    some images and second example repaired

--- matlab/sample2_tictoc.m
+++ matlab/sample2_tictoc.m
@@ -13,13 +13,14 @@ rand('seed',13);
 K=3;
 
 % problem parameters
-Ts = [2000:2000:20000, 5e4, 1e5, 5e5, 1e6]; % number of points
-n = 2; % dimension of the data
+%Ts = [2000:2000:20000,5e4,1e5,5e5,1e6,5e6,1e7]; % number of points
+Ts = [2000:2000:20000]; % number of points
+N = 2; % dimension of the data
 
-
-time_gamma0 = zeros(length(Ts),1);
-time_C = zeros(length(Ts),1);
-time_gamma = zeros(length(Ts),1);
+time_Gamma0 = zeros(length(Ts),1);
+time_all = zeros(length(Ts),1);
+time_Theta = zeros(length(Ts),1);
+time_Gamma = zeros(length(Ts),1);
 time_L = zeros(length(Ts),1);
 
 for Tidx = 1:length(Ts)
@@ -28,56 +29,66 @@ for Tidx = 1:length(Ts)
     disp(['T = ' num2str(T)])
     
     % generate problem
-    X = generate_clustering(T,n);
-    
+    X = generate_clustering(T,N);
+
+    % run K-means algorithm
+    % with one annealing step
+
     % generate random feasible gamma
-    tic
-    gamma = rand(K,T);
-    gamma_sum = sum(gamma,1);
-    for k=1:K
-        gamma(k,:) = gamma(k,:)./gamma_sum;
+    timer_Gamma0 = tic;
+    
+    Gamma = zeros(K,T);
+    for t=1:T
+        randidx = randi([1,K],1,1);
+        Gamma(randidx,t) = 1;
     end
-    time_gamma0(Tidx) = toc;
+    
+    time_Gamma0(Tidx) = toc(timer_Gamma0);
     
     it = 0;
     L = Inf;
-    C = zeros(n,K);
-    
+    Theta = zeros(N,K);
+
+    timer_all = tic;
     while it < 1000
         
-        % solve C-problem
-        tic
+        % solve Theta-problem
+        timer_Theta = tic;
         
         for k=1:K
-            sumgamma = sum(gamma(k,:));
+            sumgamma = sum(Gamma(k,:));
             if sumgamma ~= 0
-                for nn = 1:n
-                    C(nn,k) = dot(X(nn,:),gamma(k,:))/sumgamma;
+                for n = 1:N
+                    Theta(n,k) = dot(X(n,:),Gamma(k,:))/sumgamma;
                 end
             end
         end
         
-        time_C(Tidx) = time_C(Tidx) + toc;
+        time_Theta(Tidx) = time_Theta(Tidx) + toc(timer_Theta);
         
         % solve gamma problem
-        tic
-        g = zeros(size(gamma));
+        timer_Gamma = tic;
+        
+        g = zeros(size(Gamma));
         for k=1:K
-            g(k,:) = dot(X - C(:,k),X - C(:,k));
+            g(k,:) = dot(X - Theta(:,k),X - Theta(:,k));
         end
         [~,idx] = min(g,[],1);
-        gamma = zeros(K,T);
+        Gamma = zeros(K,T);
         for k=1:K
-            gamma(k,idx==k) = 1;
+            Gamma(k,idx==k) = 1;
         end
-
-        time_gamma(Tidx) = time_gamma(Tidx) + toc;
+        
+        time_Gamma(Tidx) = time_Gamma(Tidx) + toc(timer_Gamma);
         
         % compute function value
-        tic
         L_old = L;
-        L = sum(sum(bsxfun(@times,gamma,g)))/(T*n);
-        time_L(Tidx) = time_L(Tidx) + toc;
+
+        timer_L = tic;
+
+        L = sum(sum(bsxfun(@times,Gamma,g)))/(T*N);
+        
+        time_L(Tidx) = time_L(Tidx) + toc(timer_L);
         
         % check stopping criteria
         Ldelta = L_old - L;
@@ -87,14 +98,13 @@ for Tidx = 1:length(Ts)
         
         it = it + 1;
     end
-    
-    time_C(Tidx) = time_C(Tidx)/it;
-    time_gamma(Tidx) = time_gamma(Tidx)/it;
+
+    time_all(Tidx) = toc(timer_all)/it;
+    time_Theta(Tidx) = time_Theta(Tidx)/it;
+    time_Gamma(Tidx) = time_Gamma(Tidx)/it;
     time_L(Tidx) = time_L(Tidx)/it;
 end
 
-gamma = [];
-X = [];
-C = [];
+clear Gamma X Theta g
 save('results/kmeans2.mat')
 
